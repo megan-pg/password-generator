f067fcc0a23e5d490ca31d7ea4b42968cebae163
Megan Pardy-Gokcu <meganpardy-gokcu@MPG2s-MacBook-Pro.local>
2020-04-17 21:09:35 -0400

    added ifs

--- script.js
+++ script.js
@@ -4,13 +4,52 @@ var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", 
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var characters = ["!", "@", "#", "$", "%", "&", "*"];
 
-var myPasswordLength = prompt("How many characters do you want in your password? (Please enter a number)");
-var myPasswordLength = alert("Do you want uppercase letters?");
-var myPasswordLength = alert("Do you want numbers?");
-var myPasswordLength = alert("Do you want symbols?");
+//Prompts and confirmations
+var myPasswordLength = prompt("How many characters do you want in your password? (Please enter a number no larger than 20)");
+var uppercaseConfirm = confirm("Do you want uppercase letters?");
+var numbersConfirm = confirm("Do you want numbers?");
+var symbolConfirm = confirm("Do you want symbols?");
+
+
+
+var passwordBuildArray = [];
+var passwordArray = [];
+
+var passLength = parseInt(myPasswordLength);
+console.log("passLength", passLength)
+
+var randomize = Math.floor(Math.random() * Math.floor);
+
+
+if (uppercaseConfirm) {
+  passwordArray = upperLetters;
+}
+if (numbersConfirm) {
+  passwordArray = numbers;
+}
+if (symbolConfirm) {
+  passwordArray = upperLetters;
+}
+if (uppercaseConfirm && numbersConfirm) {
+  passwordArray = upperLetters.concat(numbers);
+}
+if (uppercaseConfirm && symbolConfirm) {
+  passwordArray = upperLetters.concat(characters);
+}
+if (numbersConfirm && symbolConfirm) {
+  passwordArray = numbers.concat(characters);
+}
+if (uppercaseConfirm && numbersConfirm && symbolConfirm) {
+  passwordArray = upperLetters.concat(numbers)(characters);
+}
+console.log("")
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+for (var i = 0; i < passLength; i++) {
+  passwordBuildArray.push(passwordArray[randomize])
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -18,15 +57,14 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  return password;
 
 }
 
-
-//var passwordArray = [];
-//var passLength = parseInt(newPasswordLength);
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
 
 
 //buildString()
